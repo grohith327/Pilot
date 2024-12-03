540d59035616955625e4e292e8f5203dbc8cbcf3
grohith327 <grohith327@gmail.com>
2024-12-02 21:24:04 -0800

    Add dummy sidebar

--- src/pilot-frontend/app/layout.tsx
+++ src/pilot-frontend/app/layout.tsx
@@ -1,7 +1,14 @@
+"use client"
+
+
 import "@/styles/globals.css"
 import { cn } from "@/lib/utils"
+import { fontSans } from "@/lib/fonts"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+
 
 interface RootLayoutProps {
   children: React.ReactNode
@@ -15,13 +22,19 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <body
           className={cn(
             "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
           )}
         >
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <div className="relative flex min-h-screen flex-col">
-
-              <div className="flex-1">{children}</div>
-            </div>
+            <SidebarProvider>
+              <AppSidebar />
+              <main>
+                <SidebarTrigger />
+                <div className="relative flex min-h-screen flex-col">
+                  <div className="flex-1">{children}</div>
+                </div>
+              </main>
+            </SidebarProvider>
             <TailwindIndicator />
           </ThemeProvider>
         </body>
