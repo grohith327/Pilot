32102fbd44cb8fcef54a972ab2adb4399604f65f
grohith327 <grohith327@gmail.com>
2024-12-08 21:42:19 -0800

    Add project actions dropdown button

--- src/pilot-frontend/app/projects/[id]/page.tsx
+++ src/pilot-frontend/app/projects/[id]/page.tsx
@@ -1,4 +1,13 @@
+import { ChevronDownIcon } from "lucide-react"
+
 import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import { Separator } from "@/components/ui/separator"
 import { ElementCard } from "@/components/element-card"
 
@@ -49,26 +58,41 @@ export default function ProjectPage() {
       <div className="flex justify-between items-start">
         <div>
           <h1 className="text-2xl font-bold">{dummyProjectData.name}</h1>
-          <p className="text-gray-500 text-sm mt-4">
-            {dummyProjectData.description}
-          </p>
+          <div className="flex items-center space-x-2">
+            <p className="text-gray-500 text-sm mt-4">
+              {dummyProjectData.description}
+            </p>
+            <Badge
+              variant={
+                dummyProjectData.status === "Active" ? "default" : "secondary"
+              }
+              className="mt-4"
+            >
+              {dummyProjectData.status}
+            </Badge>
+          </div>
         </div>
 
         <div className="text-right space-y-4">
-          <Badge
-            variant={
-              dummyProjectData.status === "Active" ? "default" : "secondary"
-            }
-          >
-            {dummyProjectData.status}
-          </Badge>
           <p className="text-sm text-gray-500 mt-2">
             Last Modified Date: {dummyProjectData.lastModifiedDate}
           </p>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Button className="rounded-xl" size="smd" variant="secondary">
+                Actions <ChevronDownIcon className="w-4 h-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>Add Element</DropdownMenuItem>
+              <DropdownMenuItem>Deactivate Project</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
 
       <Separator className="my-4" />
+
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {dummyProjectData.elements.map((element) => (
           <ElementCard key={element.id} {...element} />
