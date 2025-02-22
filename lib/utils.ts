915febb6ea9207c6f3043e202f03ee60b4503020
grohith327 <grohith327@gmail.com>
2025-02-21 19:04:12 -0800

    remove lib from .gitignore

---
+++ lib/utils.ts
@@ -0,0 +1,17 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatDate(date: string) {
+  return new Date(date).toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  })
+}
+
+export const API_URL = process.env.API_URL || "http://localhost:8000"
+export const STAGE = process.env.STAGE
